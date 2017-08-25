9f206423d8696af5149703ef9547927106b27476
Shine Marson <ms27946@outlook.com>
2017-08-25 12:12:34 +0800

    add:deploy website

--- DotNetCore.Fundamentals/Program.cs
+++ DotNetCore.Fundamentals/Program.cs
@@ -19,6 +19,9 @@ namespace DotNetCore.Fundamentals
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseIISIntegration()
                 .UseStartup<Startup>()
                 .Build();
     }
