12da09e1baa5ef27e631891282c128548641e03e
Shine Marson <ms27946@outlook.com>
2017-10-26 19:58:28 +0800

    add: calculate object's total memory

---
+++ CalculateGCMemory.Demo/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateGCMemory.Demo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //var currentBytes = GC.GetTotalMemory(true);
+            //var obj = new object();
+            //GC.KeepAlive(obj);
+            //var objSize = GC.GetTotalMemory(true) - currentBytes;
+            //Console.WriteLine(objSize);
+
+            var array = new[] { 1, 0, 5, 11, 23, 5569, 523, 45, 7, 256, 14, 52, 35, 47, 102, 5, 485, 7, 165, 4654, 231, 54, 56, 156, 156, 42, 12, 159, 4789, 7, 894, 21, 321, 64, 54, 21, 54, 54, 231, 2314 };
+            new SortHelper().QuickSort(array);
+            var str = "";
+            Array.ForEach(array, item =>
+            {
+                str += item + " ";
+            });
+            Console.WriteLine(str);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CalculateGCMemory.Demo/SortHelper.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateGCMemory.Demo
+{
+    public class SortHelper
+    {
+        public void QuickSort(int[] array)
+        {
+            QuickSortInternal(array, 0, array.Length - 1);
+        }
+        private void QuickSortInternal(int[] array,int begin,int end)
+        {
+            if(begin == end)
+            {
+                return;
+            }
+            else
+            {
+                int pivot = FindPivotIndex(array, begin, end);
+                if (pivot > begin) QuickSortInternal(array, begin, pivot - 1);
+                if (pivot < end) QuickSortInternal(array, pivot + 1, end);
+            }
+        }
+        private int FindPivotIndex(int[] array, int begin, int end)
+        {
+            int pivot = begin;
+            int m = begin + 1;
+            int n = end;
+
+            while (m < end && array[pivot] >= array[m])
+            {
+                m++;
+            }
+
+            while (n > begin && array[pivot] <= array[n])
+            {
+                n--;
+            }
+
+            while (m < n)
+            {
+                int temp = array[m];
+                array[m] = array[n];
+                array[n] = temp;
+
+                while (m < end && array[pivot] >= array[m])
+                {
+                    m++;
+                }
+
+                while (n > begin && array[pivot] <= array[n])
+                {
+                    n--;
+                }
+            }
+
+            if (pivot != n)
+            {
+                int temp = array[n];
+                array[n] = array[pivot];
+                array[pivot] = temp;
+
+            }
+
+            return n;
+        }
+    }
+}
