293ca16235e184aceb881266d3624dd8827a0b54
marsonshine <ms27946@outlook.com>
2019-07-31 20:56:06 +0800

    feat: efcore 按需更新字段

---
+++ AspNetCore.EFCore/EFCoreHelpers/ExpresstionHelper.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace AspNetCore.EFCore.EFCoreHelpers
+{
+    public class ExpressionHelper
+    {
+        /// <summary>
+        /// Gets the model name from a lambda expression.
+        /// </summary>
+        /// 
+        /// <returns>
+        /// The model name.
+        /// </returns>
+        /// <param name="expression">The expression.</param>
+        public static string GetExpressionText(LambdaExpression expression)
+        {
+            Stack<string> stack = new Stack<string>();
+            Expression expression1 = expression.Body;
+            while (expression1 != null)
+            {
+                if (expression1.NodeType == ExpressionType.Call)
+                {
+                    
+                    MethodCallExpression methodCallExpression = (MethodCallExpression)expression1;
+                    if (ExpressionHelper.IsSingleArgumentIndexer((Expression)methodCallExpression))
+                    {
+                        stack.Push(ExpressionHelper.GetIndexerInvocation(Enumerable.Single<Expression>((IEnumerable<Expression>)methodCallExpression.Arguments), Enumerable.ToArray<ParameterExpression>((IEnumerable<ParameterExpression>)expression.Parameters)));
+                        expression1 = methodCallExpression.Object;
+                    }
+                    else
+                        break;
+                }
+                else if (expression1.NodeType == ExpressionType.ArrayIndex)
+                {
+                    BinaryExpression binaryExpression = (BinaryExpression)expression1;
+                    stack.Push(ExpressionHelper.GetIndexerInvocation(binaryExpression.Right, Enumerable.ToArray((IEnumerable<ParameterExpression>)expression.Parameters)));
+                    expression1 = binaryExpression.Left;
+                }
+                else if (expression1.NodeType == ExpressionType.MemberAccess)
+                {
+                    MemberExpression memberExpression = (MemberExpression)expression1;
+                    stack.Push("." + memberExpression.Member.Name);
+                    expression1 = memberExpression.Expression;
+                }
+                else if (expression1.NodeType == ExpressionType.Parameter)
+                {
+                    stack.Push(string.Empty);
+                    expression1 = (Expression)null;
+                }
+                else
+                    break;
+            }
+            if (stack.Count > 0 && string.Equals(stack.Peek(), ".model", StringComparison.OrdinalIgnoreCase))
+                stack.Pop();
+            if (stack.Count <= 0)
+                return string.Empty;
+            return Enumerable.Aggregate<string>((IEnumerable<string>)stack, (Func<string, string, string>)((left, right) => left + right)).TrimStart(new char[1]
+            {
+    '.'
+            });
+        }
+    }
+}
---
+++ AspNetCore.EFCore/EFCoreHelpers/UpdateOnlyFieldHelper.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.ChangeTracking;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AspNetCore.EFCore.EFCoreHelpers
+{
+    /// <summary>
+    /// <see cref="TEntity"/> 应该约束为实体接口标记
+    /// </summary>
+    /// <typeparam name="TEntity"></typeparam>
+    public class UpdateOnlyFieldHelper<TEntity>
+        where TEntity : class
+    {
+        public async Task<int> UpdateAsync(TEntity entity, List<string> fieldNames,CancellationToken cancellationToken = default)
+        {
+            using (var context = new MyContext())
+            {
+                if (fieldNames?.Count > 0)
+                {
+                    context.Set<TEntity>().Attach(entity);
+                    foreach (var item in fieldNames)
+                    {
+                        context.Entry<TEntity>(entity)
+                            .Property(item)
+                            .IsModified = true;
+                    }
+                }
+                else
+                    context.Entry(entity)
+                        .State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+                return await context.SaveChangesAsync(cancellationToken: cancellationToken);
+
+            }
+        }
+        public async Task UpdateAsync(TEntity entity, params Expression<Func<TEntity, object>>[] properties)
+        {
+            using (var context = new MyContext())
+            {
+                context.Attach(entity);
+                EntityEntry<TEntity> entry = context.Entry(entity);
+                foreach (var selector in properties)
+                {
+                    entry.Property(selector).IsModified = true;
+                }
+                await context.SaveChangesAsync();
+            }
+        }
+        //public async Task<int> UpdateAsync(TEntity entity,Expression<Func<TEntity,object>>[] properties)
+        //{
+        //    using (var context = new MyContext())
+        //    {
+        //        context.Set<TEntity>()
+        //            .Attach(entity).State = Microsoft.EntityFrameworkCore.EntityState.Unchanged;
+        //        foreach (var property in properties)
+        //        {
+        //            var propertyName = ExpresstionHelper.GetExpressionText(property);
+        //        }
+        //    }
+        //}
+    }
+}
